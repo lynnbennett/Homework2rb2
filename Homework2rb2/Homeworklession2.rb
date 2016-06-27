4223d42e03222c0d1eb70aa0da2e9ce5150fdd61
Phyllis Bennett <plb2100@outlook.com>
2016-06-26 22:36:47 -0400

    Add files via upload

---
+++ Homework2rb2/Homeworklession2.rb
@@ -0,0 +1,54 @@
+#def fizzbuzz(num)
+#  when num % 15 == 0 then "FizzBuzz"
+ # when num % 3  == 0 then "Fizz"
+ # when num % 5  == 0 then "Buzz"
+  #else num
+  #end
+#end
+
+#def fizz_buzz_to(limit)
+  #1.upto(limit).each do |num|
+   # puts fizzbuzz(num)
+ # end
+#end
+
+# explaination of question one 
+# this is defining the attrubute fizzbuzz if the number equals 15 then the word fizzzbuzz prints out. 
+# if the number 3 can be divided in to 15 then the word " fizz "will run 
+# last if the number 5 goes in to 15 equally then the Buzz will run 
+# line 10-12 defines the atrubute.
+
+#class Homework 
+	#attr_accessor :shout 
+	# names =  ["Gregg," "Julius," "Martha", "Tina"]
+	 	#names.each do |item|
+	 		#puts "Names all Caps"
+
+	# end
+	 # end
+
+#	 class Repeat_string
+#	 	def my_method ("house","-2"," yard", ) ,("interger")
+	# 		local_variable= house*(-2)
+	# 	end
+	 #	results : (house,(-2))
+	 		
+	 #		end
+
+	 		class calender 
+	 			attr_accessor : Holidays
+	 				Names of Holidays = ["Christmas","4th of July"," New Years"]
+	 				puts "Names of Holidays"
+
+	 	end
+
+	 	class Repeat_string
+	 		def self.class_method_name
+	 			
+	 		end
+
+What is  an instance variable ? An instance variable is a class , object or any variable of a clas 
+		
+Use online ruby documentation (ruby-doc.org ) describe a method of the interger class 
+
+The method of interger class wraps a value of interger class it like a container to give detail of a string.
